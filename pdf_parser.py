d14a4ea35e9f3164e667c6e2dd29e8823f336eb2
beomjin-lee <cluesbj@gmail.com>
2018-06-27 11:55:36 -0700

    Added new documentation

--- pdf_parser.py
+++ pdf_parser.py
@@ -1,15 +1,19 @@
 """
 PDF Parser
+----------
 
+Description
+-----------
 This program parses through a PDF and returns a txt file of the parsed PDF
 if the program is parseable, based on pdf_checker.py
 
 convert_pdf_to_txt will convert ONE PDF file, while convert_directory_to_txt
 will convert an entire directory of PDF files.
 
-@author Beom Jin Lee <cluesbj@berkeley.edu>
+Author
+------
+Beom Jin Lee <cluesbj@berkeley.edu>
 """
-
 import io
 import re
 import glob
@@ -32,9 +36,10 @@ from pdfminer.layout import LAParams
 from pdfminer.converter import PDFPageAggregator
 
 def write_to_txt(txt, filename):
-    """
-    This function writes string to TXT file
+    """ Writes string to TXT file
 
+    Variables
+    ---------
     txt : string to write
     filename : Name of the file / directory the file should be placed
     """
@@ -42,11 +47,13 @@ def write_to_txt(txt, filename):
         file.write(txt)
 
 def convert_pdf_to_txt(path, password = ""):
-    """
-    This function converts a PDF to TXT file
+    """ Converts a PDF to TXT file, if PDF can be parsed.
 
-    path : path of the file to convert to TXT
-    password : if the PDF file is locked, and needs to be unlocked, use this variable. Set to None by default
+    Variables
+    ---------
+    path:      path of the file to convert to TXT
+    password:  if the PDF file is locked, and needs to be unlocked,
+               use this variable. Set to None by default
     """
     if one_document(path):
         rsrcmgr = PDFResourceManager()
@@ -60,7 +67,8 @@ def convert_pdf_to_txt(path, password = ""):
         caching = True
         pagenos=set()
 
-        for page in PDFPage.get_pages(fp, pagenos,
+        for page in PDFPage.get_pages(fp,
+                                      pagenos,
                                       maxpages = maxpages,
                                       password = password,
                                       caching = caching,
@@ -75,10 +83,11 @@ def convert_pdf_to_txt(path, password = ""):
         write_to_txt(extracted, "{}.txt".format(path))
 
 def convert_directory_to_txt(directory):
-    """
-    This function converts an entire directory of PDF to TXT file
+    """ Converts an entire directory of PDF to TXT file
 
-    directory : directory in which many PDFs are placed
+    Variables
+    ---------
+    directory: directory in which many PDFs are placed
     """
     for filename in glob.iglob('{}*.pdf'.format(directory)):
         convert_pdf_to_txt(filename)
